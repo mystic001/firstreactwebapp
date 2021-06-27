1500aa65676e3458e15b53233796be3abafb67e5
Bamidele Emmanuel <bamideleomonayin@gmail.com>
2021-06-27 11:53:17 +0100

    comit

---
+++ src/components/Button.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Link} from 'react-router-dom'
+
+import './Button.css'
+const STYLE = ["btn--primary","btn--outline"];
+const SIZE = ["btn--medium","btn--large"];
+export const Button = ({children,type, onClick,buttonStyle, buttonSize})=>{
+
+    const checkButtonStyle = STYLE.includes(buttonStyle) ? buttonStyle : STYLE[0]
+
+    const checkButtonSize = SIZE.includes(buttonSize) ? buttonSize : SIZE[0]
+
+
+    return (<Link to ="/sign-up">
+
+        <button
+        className= {`btn ${checkButtonStyle} ${checkButtonSize}`}
+        onClick={onClick}
+        type = {type}>
+            {children}
+        </button>
+    </Link>)
+
+}
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,67 @@
 import React, {useState} from 'react';
 import {Link} from "react-router-dom";
+import {Button} from './Button';
+import './Navbar.css'
 
 function Navbar(){
     const[click, setClick] = useState(false)
+    const[button, setButton] = useState(true)
 
 
+    const showOrHideButton = ()=>{
+        if(window.innerWidth <= 960 ) setButton(false)
+        else setButton(true)
+    }
     const handleClick = ()=> setClick(!click)
-    return (
+    const closeMobileMenu = ()=> console.log("clicked")
+  
+
+    window.addEventListener("resize",showOrHideButton)
+  
+  return (
 <>
-    
-    <nav className="navBar">
-        <div className="navBar-Container">
-            <Link to="/" className="navBar-logo">
+    <nav className="navbar">
+        <div className="navbar-container">
+            <Link to="/" className="navbar-logo">
                 MYSTERIESHOUSE <i class="fas fa-grimace"></i>
             </Link>
         </div>
 
-        <div onClick={handleClick}>
+        <div className="menu-icon" onClick={handleClick}>
             <i className= {click ? "fas fa-times" : "fas fa-bars"}></i>
         </div>
-    <div></div>
+
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className = "nav-item">
+                <Link to="/" className="nav-links" onClick = {closeMobileMenu}>
+                    Home
+                </Link>
+            </li>
+
+            <li className = "nav-item">
+                <Link to="/services" className="nav-links" onClick = {closeMobileMenu}>
+                    Services
+                </Link>
+            </li>
+
+            <li className = "nav-item">
+                <Link to="/products" className="nav-links" onClick = {closeMobileMenu}>
+                    Products
+                </Link>
+            </li>
+
+            <li className = "nav-item">
+                <Link to="/aboutUs" className="nav-links" onClick = {closeMobileMenu}>
+                   About Us
+                </Link>
+            </li>
+        </ul>
+        
+
+    {button ? <Button buttonStyle = "btn--outline" buttonSize = "btn--large">SIGN UP</Button> : null}
+
+      
+        
     </nav>
     
     </>
