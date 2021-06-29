bd6730305a984f3a41a066d35b7d7b2c37033f2e
mystic <bamideleomonayin@gmail.com>
2021-06-29 19:39:58 +0000

    creating on cloud

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import '../App.css'
+import "./services.css"
 function Services() {
     return (
-        <div className = "services">
+
+<div class="grid-container">
+  <div class="grid-item">1</div>
+  <div class="grid-item">2</div>
+  <div class="grid-item">3</div>
+  <div class="grid-item">4</div>
+  <div class="grid-item">5</div>
+  <div class="grid-item">6</div>
+  <div class="grid-item">7</div>
+  <div class="grid-item">8</div>
+  <div class="grid-item">9</div>
+  <div className = "services">
             <h1>Services</h1>
         </div>
+</div>
+       
     )
 }
 
