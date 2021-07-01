48e0ee2ab1001879420673407fc686c32a200f11
Bamidele Emmanuel <bamideleomonayin@gmail.com>
2021-07-01 10:56:54 +0100

    onprem work

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import '../App.css'
-
+import '../Product.css'
 function Product() {
     return (
-        <div className = "products">
-            <h1>Dang</h1>
-        </div>
+
+        <>
+            <div className="product">
+                <h1>Products</h1>
+            </div>
+
+            <div class="grid-container">
+                <div className="grid-item1">
+                <p>I am the first box and i hope this contains me</p>
+                1</div>
+                <div className="grid-item2">Box2</div>
+                <div className="grid-item3">Box3</div>
+            </div>
+        </>
+
     )
 }
 
