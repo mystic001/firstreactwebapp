0c81fc37f6e73f54569f5471493d75e2f5538d02
mystic <bamideleomonayin@gmail.com>
2021-07-14 20:03:33 +0000

    adding services

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -3,6 +3,7 @@ import '../App.css'
 import '../services.css'
 function Services() {
     return (
+<>
 
 <div class="grid-container">
   <div class="item1">Header</div>
@@ -11,6 +12,28 @@ function Services() {
   <div class="item4">Right</div>
   <div class="item5">Footer</div>
 </div>
+
+<div class="grid-containersecond">
+  <div class="itemone">Box 1</div>
+  <div class="itemtwo">Box 2</div>
+  <div class="itemthree">Box 3</div>  
+</div>
+
+
+<div class="grid-containerthird">
+  <div class="box1">Box 1</div>
+  <div class="box2">Box 2</div>
+  <div class="box3">Box 3</div> 
+</div>
+
+
+<div class="grid-containerfourth">
+  <div class="bxf1">Box 1</div>
+  <div class="bxf2">Box 2</div>
+  <div class="bxf3">Box 3</div> 
+</div>
+</>
+
        
     )
 }
